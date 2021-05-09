9246517ccda9600c25a5bef2681f3f5bf98a16a5
Ish Kapoor <>
2021-05-09 21:02:19 +0000

    adding HD Skybox as background

--- script.js
+++ script.js
@@ -1,89 +1,99 @@
-		import { VRButton } from './js/VRButton.js';
+import { VRButton } from './js/VRButton.js';
 
-		const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+scene.background = new THREE.CubeTextureLoader()
+.setPath('skybox_1/').load([
+	'front.png',
+	'back.png',
+	'top.png',
+	'bottom.png',
+	'left.png',
+	'right.png'
+]);
 
-		const renderer = new THREE.WebGLRenderer();
-		renderer.setPixelRatio(window.devicePixelRatio);
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		document.body.appendChild(renderer.domElement);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-		// Sphere 1
-		const geometry = new THREE.SphereGeometry(10, 10, 10);
-		const material = new THREE.MeshNormalMaterial({
-			wireframe: true
-		});
-		const sphere = new THREE.Mesh(geometry, material);
-		// scene.add(sphere);
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-		// Sphere 2
-		const geometry2 = new THREE.SphereGeometry(10, 10, 10);
-		const material2 = new THREE.MeshNormalMaterial({
-			wireframe: true
-		});
-		const sphere2 = new THREE.Mesh(geometry2, material2);
-		sphere2.position.x = 40
-		scene.add(sphere2);
+// Sphere 1
+const geometry = new THREE.SphereGeometry(10, 10, 10);
+const material = new THREE.MeshNormalMaterial({
+	wireframe: true
+});
+const sphere = new THREE.Mesh(geometry, material);
+// scene.add(sphere);
 
-		// Sphere 2
-		const geometry3 = new THREE.SphereGeometry(10, 10, 10);
-		const material3 = new THREE.MeshNormalMaterial({
-			wireframe: true
-		});
-		const sphere3 = new THREE.Mesh(geometry3, material3);
-		sphere3.position.x = -40
-		scene.add(sphere3);
+// Sphere 2
+const geometry2 = new THREE.SphereGeometry(10, 10, 10);
+const material2 = new THREE.MeshNormalMaterial({
+	wireframe: true
+});
+const sphere2 = new THREE.Mesh(geometry2, material2);
+sphere2.position.x = 40
+scene.add(sphere2);
 
-		camera.position.z = 70;
+// Sphere 3
+const geometry3 = new THREE.SphereGeometry(10, 10, 10);
+const material3 = new THREE.MeshNormalMaterial({
+	wireframe: true
+});
+const sphere3 = new THREE.Mesh(geometry3, material3);
+sphere3.position.x = -40
+scene.add(sphere3);
 
-		const domEvents = new THREEx.DomEvents(camera, renderer.domElement);
+camera.position.z = 70;
 
-		domEvents.addEventListener(sphere, 'click', event => {
-			material.wireframe = false
-		})
+const domEvents = new THREEx.DomEvents(camera, renderer.domElement);
 
-		domEvents.addEventListener(sphere2, 'mouseover',event => {
-			sphere2.scale.set(2, 2, 2)
-		})
+domEvents.addEventListener(sphere, 'click', event => {
+	material.wireframe = false
+})
 
-		domEvents.addEventListener(sphere2, 'mouseout',event => {
-			sphere2.scale.set(1, 1, 1)
-		})
+domEvents.addEventListener(sphere2, 'mouseover',event => {
+	sphere2.scale.set(2, 2, 2)
+})
 
-		const controls = new THREE.OrbitControls(camera, renderer.domElement);
-		controls.minDistance = 1
-		controls.maxDistance = 1000
+domEvents.addEventListener(sphere2, 'mouseout',event => {
+	sphere2.scale.set(1, 1, 1)
+})
 
-		const vrbutton = VRButton.createButton(renderer);
-		document.body.appendChild(vrbutton);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.minDistance = 1
+controls.maxDistance = 1000
 
-		renderer.xr.enabled = true;
-  		document.body.appendChild(VRButton.createButton(renderer));
+const vrbutton = VRButton.createButton(renderer);
+document.body.appendChild(vrbutton);
 
-		const animate = () => {
+renderer.xr.enabled = true;
+document.body.appendChild(VRButton.createButton(renderer));
 
-			//Animate Sphere 1
-			sphere.rotation.x += 0.02;
-			sphere.rotation.y += 0.02;
+const animate = () => {
 
-			//Animate Sphere 2
-			sphere2.rotation.x += 0.02;
-			sphere2.rotation.y += 0.02;
+	//Animate Sphere 1
+	sphere.rotation.x += 0.02;
+	sphere.rotation.y += 0.02;
 
-			//Animate Sphere 3
-			sphere3.rotation.x += 0.02;
-			sphere3.rotation.y += 0.02;
+	//Animate Sphere 2
+	sphere2.rotation.x += 0.02;
+	sphere2.rotation.y += 0.02;
 
-			controls.update();
+	//Animate Sphere 3
+	sphere3.rotation.x += 0.02;
+	sphere3.rotation.y += 0.02;
 
-			requestAnimationFrame(animate);
-			// renderer.setAnimationLoop(function(time) {
-			// 	renderer.render(scene, camera);
-			// });
-			renderer.render(scene, camera);
-		}
-		renderer.setAnimationLoop(function() {
-			renderer.render(scene, camera);
-		});
+	controls.update();
 
-		animate();
+	requestAnimationFrame(animate);
+	// renderer.setAnimationLoop(function(time) {
+	// 	renderer.render(scene, camera);
+	// });
+	renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(function() {
+	renderer.render(scene, camera);
+});
+
+animate();
